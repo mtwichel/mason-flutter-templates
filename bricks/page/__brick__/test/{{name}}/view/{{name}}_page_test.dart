ffe75c939158fd48c46756178d0b0d5886aba473
BruceClark <brucewestonmt@gmail.com>
2021-07-28 17:17:34 -0600

    chore: null safety

--- bricks/page/__brick__/test/{{name}}/view/{{name}}_page_test.dart
+++ bricks/page/__brick__/test/{{name}}/view/{{name}}_page_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:{{#snakeCase}}{{package_name}}{{/snakeCase}}:{{#snakeCase}}{{name}}{{/snakeCase}}/{{#snakeCase}}{{name}}{{/snakeCase}}.dart';
+import 'package:{{#snakeCase}}{{package_name}}{{/snakeCase}}/{{#snakeCase}}{{name}}{{/snakeCase}}/{{#snakeCase}}{{name}}{{/snakeCase}}.dart';
 
 void main() {
   group('{{# pascalCase}}{{name}}{{/ pascalCase}}Page', () {
